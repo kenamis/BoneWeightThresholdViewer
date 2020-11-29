b450ddab90ad5385bf1a5ea2b2ba1ee00c7ebbdc
Kenan <kenan.chabuk@gmail.com>
2020-11-29 10:39:20 -0800

    Add files via upload

---
+++ Editor/BoneWeightThresholdViewerEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RainyReignGames
+{
+    [CustomEditor(typeof(BoneWeightThresholdViewer))]
+    public class BoneWeightThresholdViewerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {           
+            if(SceneView.lastActiveSceneView != null && SceneView.lastActiveSceneView.drawGizmos == false)
+            {
+                EditorGUILayout.HelpBox("Gizmos are not enabled in SceneView.  You will be unable to see the vertex gizmos!", MessageType.Warning);
+            }
+
+            base.OnInspectorGUI();
+        }
+    }
+}
